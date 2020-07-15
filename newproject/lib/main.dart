38939aaefe27bc887b932d720574f0cd43e46475
Rafael <ralf_teles@hotmail.com>
2020-07-15 15:10:09 -0300

    adicionando projeto

---
+++ newproject/lib/main.dart
@@ -0,0 +1,125 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:newproject/models/item.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    // items.add(Item(title: "Banana", done: false));
+    // items.add(Item(title: "Abacate", done: true));
+    // items.add(Item(title: "Laranja", done: true));
+    // items.add(Item(title: "Limão", done: true));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void addItem() {
+    if (newTaskCtrl.text.isEmpty) return;
+    setState(() {
+      widget.items.add(Item(
+        title: newTaskCtrl.text,
+        done: false,
+      ));
+      newTaskCtrl.text = "";
+      save();
+    });
+  }
+
+  void remove(index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+    save();
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(color: Colors.white, fontSize: 18),
+          decoration: InputDecoration(
+              labelText: "Nome da tarefa",
+              labelStyle: TextStyle(color: Colors.white)),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctx, int index) {
+          final item = widget.items[index];
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                  save();
+                });
+              },
+            ),
+            key: Key(item.title),
+            background: Container(
+              color: Colors.red.withOpacity(0.2),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: addItem, 
+          child: Icon(Icons.add),
+          backgroundColor: Colors.pink),
+    );
+  }
+}
